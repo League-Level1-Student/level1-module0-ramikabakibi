4192ed2c12be04d828135a05c407712d6d6e06e9
League Student <league@iMac-5.attlocal.net>
2019-08-12 16:32:13 -0700

    Houses(IP)

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,15 +1,29 @@
 package _01_houses;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	static Robot rob= new Robot();
-	
-void drawHouse(int height){
+	void drawHouse(Color color) {
+		Random rand= new Random();
+		int x=rand.nextInt(3);
+		if(x==0) {
+			rob.setPenColor(Color.red);
+		}
+		if(x==1) {
+			rob.setPenColor(Color.GREEN);
+		}
+		if(x==2) {
+			rob.setPenColor(Color.blue);
+		}
+		//on number 6
+	}
+void drawHouse(int height, Color color ){
 	rob.penDown();
-	rob.setPenColor(Color.red);
+	rob.setPenColor(color);
 	rob.setPenWidth(5);
 	rob.setSpeed(40);
 	rob.move(height);
@@ -23,17 +37,47 @@ void drawHouse(int height){
 	rob.move(50);
 	rob.turn(270);
 }
+void drawHouse(String height) {
+	if(height.equals("small")) {
+		drawHouse(60);
+	}
+	else if(height.equals("medium")){
+		drawHouse(120);
+	}
+	else if(height.equals("large")) {
+		drawHouse(250);
+	}
+ }
+
+void drawHouses() {
+	Houses b =new Houses();
+	rob.setX(10);
+	rob.setY(500);
+
+	Random rand= new Random();
+	for(int i=0;i<9;i++) {
+		int j=rand.nextInt(3);
+		if(j==0) {
+		drawHouse("small");
+		}
+		else if(j==1) {
+			drawHouse("medium");
+		}
+		else if(j==2) {
+		drawHouse("large");
+		}
+		
+		
+	}
+
+
+		}
+
+
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-Houses b =new Houses();
-rob.setX(10);
-rob.setY(500);
-for(int i=0;i<10;i++) {
-	b.drawHouse();
-//I'm on number 4	
-}
+new Houses().drawHouses();
 
 
 	}
